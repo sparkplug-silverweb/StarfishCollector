76d135caacd4db45e4e0a769ec4fac44945c70d7
lester.serdenia@gmail.com <lester.serdenia@gmail.com>
2019-07-28 09:39:21 +0800

    Create GameBeta.java

---
+++ core/src/com/mygdx/game/GameBeta.java
@@ -0,0 +1,40 @@
+package com.mygdx.game;
+
+/**
+ *
+ * @author lserd
+ */
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public abstract class GameBeta extends Game
+{
+    protected Stage mainStage;
+    
+    @Override
+    public void create()
+    {
+        mainStage = new Stage();
+        initialize();
+    }
+    
+    public abstract void initialize() ;
+    
+    @Override
+    public void render()
+    {
+        float dt = Gdx.graphics.getDeltaTime();
+        // act method
+        mainStage.act(dt);
+        // defined by user
+        update(dt);
+        // clear the screen
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // draw the graphics
+        mainStage.draw();
+    }
+    public abstract void update(float dt) ;
+}
